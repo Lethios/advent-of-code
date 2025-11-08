c2dfcbde0145c1dde69105814e7e6df3b6ab7f5e
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-08 21:14:26 +0530

    Add floor calculation logic in main function
    Calculate the final floor based on input characters.

--- 2015/day01.rs
+++ 2015/day01.rs
@@ -1,1 +1,16 @@
 // https://adventofcode.com/2015/day/1
+
+fn main() {
+    let input: &str = "<INPUT>";
+    let mut floor: i32 = 0;
+    
+    for chr in input.chars() {
+        match chr {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => {}
+        }
+    }
+    
+    println!("Floor {floor}");
+}
