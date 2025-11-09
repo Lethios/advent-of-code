42a35d95f3d25cc8a0b9d68e808b85fc052571f3
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-09 07:52:30 +0530

    Can be improved

--- 2017/day01.rs
+++ 2017/day01.rs
@@ -1,1 +1,40 @@
+fn part1(input: &str) -> u32 {
+    let mut prev: u32 = input.chars().last().unwrap().to_digit(10).unwrap();
+    let mut sum: u32 = 0;
 
+    for chr in input.chars() {
+        let digit: u32 = chr.to_digit(10).unwrap();
+
+        if digit == prev {
+            sum += digit;
+        }
+        prev = digit;
+    }
+
+    sum
+}
+
+fn part2(input: &str) -> u32 {
+    let len: usize = input.len();
+    let mut sum: u32 = 0;
+
+    for (i, chr) in input.chars().enumerate() {
+        let digit: u32 = chr.to_digit(10).unwrap();
+        let index: usize = (i + (len / 2)) % len;
+
+        let half = input.chars().nth(index).unwrap().to_digit(10).unwrap();
+
+        if digit == half {
+            sum += digit;
+        }
+    }
+
+    sum
+}
+
+fn main() {
+    let input: &str = "<INPUT>";
+
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
