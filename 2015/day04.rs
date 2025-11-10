4e5dcc8f28a67f9348f30e4647908270cdaf1856
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-10 17:54:10 +0530

    Implement part2 function for MD5 hash calculation
    Add part2 function to compute a key based on MD5 hash.

--- 2015/day04.rs
+++ 2015/day04.rs
@@ -23,8 +23,30 @@ fn part1(input: &str) -> u32 {
     key
 }
 
+fn part2(input: &str) -> u32 {
+    let mut number: u32 = 0;
+
+    let key: u32 = loop {
+        let mut hasher = Md5::new();
+
+        let payload: String = format!("{input}{number}");
+        hasher.update(payload.as_bytes());
+
+        let hash = hasher.finalize();
+        let hash_string: String = format!("{:x}", hash);
+
+        if &hash_string[..6] == "000000" {
+            break number;
+        }
+        number += 1;
+    };
+
+    key
+}
+
 fn main() {
     let input: String = String::from("bgvyzdsv");
 
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
