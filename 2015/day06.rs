e4cf9236469d2e5f69d6f8423c7923710ae4e495
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-11 20:43:11 +0530

    Update day06.rs

--- 2015/day06.rs
+++ 2015/day06.rs
@@ -1,1 +1,130 @@
+// https://adventofcode.com/2015/day/6
 
+use std::fs;
+
+fn part1(input: &str) -> u32 {
+    let mut light_grid: Vec<[bool; 1000]> = vec![[false; 1000]; 1000];
+
+    for line in input.lines() {
+        let args: Vec<&str> = line.split(" ").collect();
+
+        let start_coords: Vec<u32> = args[if args[0] == "turn" { 2 } else { 1 }]
+            .split(",")
+            .map(|num| num.parse().unwrap())
+            .collect();
+        let end_coords: Vec<u32> = args[args.len() - 1]
+            .split(",")
+            .map(|num| num.parse().unwrap())
+            .collect();
+
+        match args[0] {
+            "turn" => match args[1] {
+                "on" => {
+                    for row in start_coords[0]..=end_coords[0] {
+                        for column in start_coords[1]..=end_coords[1] {
+                            light_grid[row as usize][column as usize] = true;
+                        }
+                    }
+                }
+                "off" => {
+                    for row in start_coords[0]..=end_coords[0] {
+                        for column in start_coords[1]..=end_coords[1] {
+                            light_grid[row as usize][column as usize] = false;
+                        }
+                    }
+                }
+                _ => {}
+            },
+            "toggle" => {
+                for row in start_coords[0]..=end_coords[0] {
+                    for column in start_coords[1]..=end_coords[1] {
+                        light_grid[row as usize][column as usize] =
+                            if light_grid[row as usize][column as usize] {
+                                false
+                            } else {
+                                true
+                            };
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    let mut lit_count: u32 = 0;
+    for row in 0..1000 {
+        for column in 0..1000 {
+            lit_count = if light_grid[row][column] {
+                lit_count + 1
+            } else {
+                lit_count
+            };
+        }
+    }
+
+    lit_count
+}
+
+fn part2(input: &str) -> i32 {
+    let mut light_grid: Vec<[i32; 1000]> = vec![[0; 1000]; 1000];
+
+    for line in input.lines() {
+        let args: Vec<&str> = line.split(" ").collect();
+
+        let start_coords: Vec<u32> = args[if args[0] == "turn" { 2 } else { 1 }]
+            .split(",")
+            .map(|num| num.parse().unwrap())
+            .collect();
+        let end_coords: Vec<u32> = args[args.len() - 1]
+            .split(",")
+            .map(|num| num.parse().unwrap())
+            .collect();
+
+        match args[0] {
+            "turn" => match args[1] {
+                "on" => {
+                    for row in start_coords[0]..=end_coords[0] {
+                        for column in start_coords[1]..=end_coords[1] {
+                            light_grid[row as usize][column as usize] += 1;
+                        }
+                    }
+                }
+                "off" => {
+                    for row in start_coords[0]..=end_coords[0] {
+                        for column in start_coords[1]..=end_coords[1] {
+                            if light_grid[row as usize][column as usize] > 0 {
+                                light_grid[row as usize][column as usize] -= 1;
+                            } else {
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            },
+            "toggle" => {
+                for row in start_coords[0]..=end_coords[0] {
+                    for column in start_coords[1]..=end_coords[1] {
+                        light_grid[row as usize][column as usize] += 2;
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    let mut lit_count: i32 = 0;
+    for row in 0..1000 {
+        for column in 0..1000 {
+            lit_count += light_grid[row][column];
+        }
+    }
+
+    lit_count
+}
+
+fn main() {
+    let input: String = fs::read_to_string("input.txt").expect("Failed to read input file");
+
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
