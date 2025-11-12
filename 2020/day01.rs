9dde79710249afad417b7a4cfef74eb8abe3c42d
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-12 13:40:35 +0530

    Add part2 function for 2020 challenge
    Implemented part2 function to calculate product of three numbers that sum to 2020.

--- 2020/day01.rs
+++ 2020/day01.rs
@@ -19,15 +19,31 @@ fn part1(input: &str) -> u32 {
     product
 }
 
-// fn part2(input: &str) -> u32 {
+fn part2(input: &str) -> u32 {
+    let numbers: Vec<u32> = input.lines().filter_map(|line| line.parse().ok()).collect();
 
-// }
+    for i in 0..numbers.len() {
+        let anchor: u32 = numbers[i];
+        let target: u32 = 2020 - anchor;
+
+        let mut difference_set: HashSet<u32> = HashSet::new();
+
+        for j in i + 1..numbers.len() {
+            let difference: u32 = target.saturating_sub(numbers[j]);
+            if difference_set.contains(&difference) {
+                return Some(anchor * numbers[j] * difference).expect("Error");
+            }
+            difference_set.insert(numbers[j]);
+        }
+    }
+
+    0
+}
 
 fn main() {
     let input: String = fs::read_to_string("input.txt").expect("Failed to read input file");
 
     println!("Part 1: {}", part1(&input));
-    // println!("Part 2: {}", part2(&input));
+    println!("Part 2: {}", part2(&input));
 }
-
 
