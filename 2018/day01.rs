de660471c23abce4e39eb7719ac6181228bd9a9e
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-12 11:28:24 +0530

    Add frequency calculation for Advent of Code 2018 Day 1
    Implement part1 and part2 functions to calculate frequency.

--- 2018/day01.rs
+++ 2018/day01.rs
@@ -1,1 +1,41 @@
+// https://adventofcode.com/2018/day/1
+
+use std::{collections::HashSet, fs};
+
+fn part1(input: &str) -> i32 {
+    let mut frequency: i32 = 0;
+
+    for line in input.lines() {
+        let number: i32 = line.parse().expect("Invalid integer");
+        frequency += number;
+    }
+
+    frequency
+}
+
+fn part2(input: &str) -> i32 {
+    let mut frequency: i32 = 0;
+    let mut frequency_set: HashSet<i32> = HashSet::new();
+    frequency_set.insert(0);
+
+    loop {
+        for line in input.lines() {
+            let number: i32 = line.parse().expect("Invalid integer");
+            frequency += number;
+
+            if frequency_set.contains(&frequency) {
+                return frequency;
+            }
+            frequency_set.insert(frequency);
+        }
+    }
+}
+
+fn main() {
+    let input: String = fs::read_to_string("input.txt").expect("Failed to read input file");
+
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
 
