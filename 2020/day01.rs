b7b2affe477c2ea43d5ae5732d6dc79b46eb833f
Lethios <204111230+Lethios@users.noreply.github.com>
2025-11-12 12:19:14 +0530

    Add part1 function for Advent of Code Day 1
    Implement part1 function to calculate product of two numbers that sum to 2020.

--- 2020/day01.rs
+++ 2020/day01.rs
@@ -1,1 +1,33 @@
+// https://adventofcode.com/2019/day/1
+
+use std::{collections::HashSet, fs};
+
+fn part1(input: &str) -> u32 {
+    let mut difference_set: HashSet<u32> = HashSet::new();
+    let mut product: u32 = 0;
+
+    for line in input.lines() {
+        let number: u32 = line.parse().expect("Invalid number");
+        let difference: u32 = 2020 - number;
+
+        if difference_set.contains(&number) {
+            product = number * difference;
+        }
+        difference_set.insert(difference);
+    }
+
+    product
+}
+
+// fn part2(input: &str) -> u32 {
+
+// }
+
+fn main() {
+    let input: String = fs::read_to_string("input.txt").expect("Failed to read input file");
+
+    println!("Part 1: {}", part1(&input));
+    // println!("Part 2: {}", part2(&input));
+}
+
 
